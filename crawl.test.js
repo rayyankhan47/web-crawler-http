d5eb63931c66005523046f5bfa85935afa70ab0e
AhmedSalafi <ryankhann47@gmail.com>
2026-02-27 02:46:06 -0500

    create normalizeURL and a small test suite for it

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,30 @@
 const { normalizeURL } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
-test("normalizeURL", () => {
-    const input = "";
+test("normalizeURL strip protocol", () => {
+    const input = "https://khanrayyan.com/somepath";
     const actual = normalizeURL(input);
-    const expected = "";
+    const expected = "khanrayyan.com/somepath";
+    expect(actual).toEqual(expected);
+})
+
+test("normalizeURL strip trailing slash", () => {
+    const input = "https://khanrayyan.com/somepath/";
+    const actual = normalizeURL(input);
+    const expected = "khanrayyan.com/somepath";
+    expect(actual).toEqual(expected);
+})
+
+test("normalizeURL capitals", () => {
+    const input = "https://KHANrayyan.com/somepath/";
+    const actual = normalizeURL(input);
+    const expected = "khanrayyan.com/somepath";
+    expect(actual).toEqual(expected);
+})
+
+test("normalizeURL strip http", () => {
+    const input = "http://KHANrayyan.com/somepath/";
+    const actual = normalizeURL(input);
+    const expected = "khanrayyan.com/somepath";
     expect(actual).toEqual(expected);
 })
